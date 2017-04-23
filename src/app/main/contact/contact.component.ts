e96379b96c17ae9eb619ca509ee80f2eca65bfa0
Ivelin  Ivanov <ivelinivanov@Ivelins-MacBook-Pro.local>
2017-04-23 17:57:04 +0200

    added GoogleMaps
    MERGE

--- src/app/main/contact/contact.component.ts
+++ src/app/main/contact/contact.component.ts
@@ -1,10 +1,28 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+// import {MdSnackBar} from '@angular/material';
 
+export interface User {
+  email: any;
+  name: string;
+  message: string;
+}
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
+  styles: [`
+    input.ng-invalid {
+      border: 1px solid red;
+    }
+    `]
 })
 export class ContactComponent implements OnInit {
+
+  private user: User = {
+    email: '',
+    name: '',
+    message: ''
+  };
 
   @Input()
   latitude: number;
@@ -20,4 +38,7 @@ export class ContactComponent implements OnInit {
   ngOnInit() {
   }
 
+  onSubmit(form: NgForm) {
+    console.log('Form to be submitted ! =>', form.value);
+  }
 }
