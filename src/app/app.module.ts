2125283183f83ab66d9c4c49e6842071d5e725a3
Ivelin Ivanov <ivelin.ivanovxps@gmail.com>
2018-11-01 19:28:35 +0100

    commit => $package.json $navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -56,14 +56,7 @@ import * as firebase from 'firebase';
   ],
   imports: [
     environment.production ? ServiceWorkerModule.register('/ngsw-worker.js') : [],
-    AngularFireModule.initializeApp({
-      apiKey: 'AIzaSyA5_j1wX-uyz1MJCa5MnAOO08F1EVrca3A',
-      authDomain: 'dorex-prod.firebaseapp.com',
-      databaseURL: 'https://dorex-prod.firebaseio.com',
-      projectId: 'dorex-prod',
-      storageBucket: 'dorex-prod.appspot.com',
-      messagingSenderId: '342130392500'
-    }),
+    AngularFireModule.initializeApp(environment.firebase),
     Angular2ImageGalleryModule,
     AngularFireDatabaseModule,
     AngularFireAuthModule,
